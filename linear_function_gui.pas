a3832811701648030ce56ea54ce516b8baa69e08
Shinbatsu <nojic132@gmail.com>
2015-05-22 18:35:09 +0300

    add exit function

--- linear_function_gui.pas
+++ linear_function_gui.pas
@@ -1,14 +1,16 @@
 program LinearFunctionGraphGUI;
 
 uses
-  Graph, Crt;
+  Graph, Crt, SysUtils;
 
 var
   gd, gm: Integer;
   m, b: Real;
   x: Integer;
   y: Real;
-  px, py: Integer;
+  prev_px, prev_py, px, py: Integer;
+  ch: Char;
+  firstPoint: Boolean;
 
 const
   X_SCALE = 20;
@@ -21,10 +23,10 @@ var
   i: Integer;
 begin
   SetColor(LightGray);
-  // Вертикальные линии
+
   for i := 0 to GetMaxX div X_SCALE do
     Line(i * X_SCALE, 0, i * X_SCALE, GetMaxY);
-  // Горизонтальные линии
+
   for i := 0 to GetMaxY div Y_SCALE do
     Line(0, i * Y_SCALE, GetMaxX, i * Y_SCALE);
 end;
@@ -32,18 +34,45 @@ end;
 procedure DrawAxes;
 begin
   SetColor(White);
-  // X-axis
+
   Line(0, Y_OFFSET, GetMaxX, Y_OFFSET);
-  // Y-axis
+
   Line(X_OFFSET, 0, X_OFFSET, GetMaxY);
 end;
 
+procedure DrawFunction(m, b: Real);
 begin
-  write('Введите коэффициент наклона (m): ');
-  readln(m);
-  write('Введите точку пересечения с осью Y (b): ');
-  readln(b);
+  SetColor(Green);
+  firstPoint := True;
 
+  for x := -X_OFFSET to X_OFFSET do
+  begin
+    y := m * (x / X_SCALE) + b;
+    px := X_OFFSET + x;
+    py := Y_OFFSET - Round(y * Y_SCALE);
+
+    if (px >= 0) and (px <= GetMaxX) and (py >= 0) and (py <= GetMaxY) then
+    begin
+      if firstPoint then
+        firstPoint := False
+      else
+        Line(prev_px, prev_py, px, py);
+      prev_px := px;
+      prev_py := py;
+    end;
+  end;
+end;
+
+procedure DisplayEquation(m, b: Real);
+var
+  eq: String;
+begin
+  SetColor(White);
+  eq := Format('y = %.2fx + %.2f', [m, b]);
+  OutTextXY(10, 10, eq);
+end;
+
+begin
   gd := Detect;
   InitGraph(gd, gm, '');
 
@@ -53,21 +82,22 @@ begin
     halt(1);
   end;
 
-  DrawGrid;
-  DrawAxes;
+  repeat
+    ClearDevice;
 
-  SetColor(Green);
-  for x := -X_OFFSET to X_OFFSET do
-  begin
-    y := m * (x / X_SCALE) + b;
-    px := X_OFFSET + x;
-    py := Y_OFFSET - Round(y * Y_SCALE);
+    write('Введите коэффициент наклона (m): ');
+    readln(m);
+    write('Введите точку пересечения с осью Y (b): ');
+    readln(b);
 
-    if (px >= 0) and (px <= GetMaxX) and (py >= 0) and (py <= GetMaxY) then
-      PutPixel(px, py, Green);
-  end;
+    DrawGrid;
+    DrawAxes;
+    DrawFunction(m, b);
+    DisplayEquation(m, b);
 
-  OutTextXY(10, 10, 'Нажмите любую клавишу...');
-  readln;
+    OutTextXY(10, GetMaxY - 20, 'Esc - выход');
+    ch := ReadKey;
+  until ch = #27;
+
   CloseGraph;
 end.
